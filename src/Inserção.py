2db45761b6d6cb2c81d771a158799c3140f22809
Pedro Cortez <pedro.cortez.alves@gmail.com>
2026-08-18 13:38:14 -0300

    Refactor functions to use 'ultima_linha' parameter
    Updated functions to use 'ultima_linha' instead of 'linha_busca' for better clarity and reduced complexity. Added a line to update the cell 'M1' with the new last line number.

--- src/Inserção.py
+++ src/Inserção.py
@@ -2,26 +2,20 @@ from openpyxl import load_workbook
 import os
 from openpyxl.styles import Alignment,  PatternFill
 
-def atualizar_dia(tempo,periodo, linha_busca, situacao,wb,aba,nome_arquivo):
+def atualizar_dia(tempo,periodo, ultima_linha, situacao,wb,aba,nome_arquivo):
     
-    prog = 1
-
-    while True:
-        if aba.cell(row=linha_busca-1, column=prog).value != None:
-            prog += 1
-        else:
-            break   
-        
-    qtd=aba.cell(row=linha_busca, column=1, value=prog)
+    prog = aba.cell(row=ultima_linha, column=1).value+1  
+    
+    qtd=aba.cell(row=ultima_linha, column=1, value=prog)
     qtd.alignment= Alignment(horizontal="center", vertical="center")
-        
-    sit=aba.cell(row=linha_busca-1, column=prog, value=situacao)
+
+    sit=aba.cell(row=ultima_linha-1, column=prog, value=situacao)
     sit.alignment= Alignment(horizontal="center", vertical="center")
 
-    tem=aba.cell(row=linha_busca-2, column=prog, value=int(tempo))
+    tem=aba.cell(row=ultima_linha-2, column=prog, value=int(tempo))
     tem.alignment= Alignment(horizontal="center", vertical="center")
 
-    per=aba.cell(row=linha_busca-3, column=prog, value=periodo)
+    per=aba.cell(row=ultima_linha-3, column=prog, value=periodo)
     per.alignment= Alignment(horizontal="center", vertical="center")
     
     if periodo == "M":
@@ -33,12 +27,13 @@ def atualizar_dia(tempo,periodo, linha_busca, situacao,wb,aba,nome_arquivo):
     
     wb.save(nome_arquivo)
     
-def novo_dia(tempo,periodo, linha_busca, situacao,wb,aba,Dia_hoje,nome_arquivo):
+def novo_dia(tempo,periodo, ultima_linha, situacao,wb,aba,Dia_hoje,nome_arquivo):
 
-    dia=aba.cell(row=linha_busca+1, column=1, value=Dia_hoje)
+    dia=aba.cell(row=ultima_linha+1, column=1, value=Dia_hoje)
     dia.alignment= Alignment(horizontal="center", vertical="center")
     dia.fill= PatternFill(start_color="B5EDBA", end_color="B5EDBA", fill_type="solid")
-    per=aba.cell(row=linha_busca+2, column=1, value=periodo)
+
+    per=aba.cell(row=ultima_linha+2, column=1, value=periodo)
     per.alignment=Alignment(horizontal="center", vertical="center")
 
     if periodo == "M":
@@ -48,13 +43,13 @@ def novo_dia(tempo,periodo, linha_busca, situacao,wb,aba,Dia_hoje,nome_arquivo):
     else:
         per.fill= PatternFill(start_color="7B6DFD", end_color="7B6DFD", fill_type="solid")
 
-    tem=aba.cell(row=linha_busca+3, column=1, value=int(tempo))
+    tem=aba.cell(row=ultima_linha+3, column=1, value=int(tempo))
     tem.alignment= Alignment(horizontal="center", vertical="center")
 
-    sit=aba.cell(row=linha_busca+4, column=1, value=situacao)
+    sit=aba.cell(row=ultima_linha+4, column=1, value=situacao)
     sit.alignment= Alignment(horizontal="center", vertical="center")
-    aba.cell(row=linha_busca+5, column=1, value=1)
-
+    aba.cell(row=ultima_linha+5, column=1, value=1)
+    aba["M1"] = ultima_linha+5
     wb.save(nome_arquivo)
 
 
